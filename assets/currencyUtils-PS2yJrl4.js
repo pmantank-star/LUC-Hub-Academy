aa5157235e2b80098157bcfe7de36a823e5a8345
pmantank-star <pmantank@gmail.com>
2026-07-30 13:44:09 +0100

    Add files via upload

---
+++ assets/currencyUtils-PS2yJrl4.js
@@ -0,0 +1,2 @@
+const t=r=>r*10,a=r=>Math.floor(r/10),o=r=>`${new Intl.NumberFormat().format(r)} LUC`,n=r=>`₦${new Intl.NumberFormat().format(r)}`;export{o as a,n as f,t as l,a as n};
+//# sourceMappingURL=currencyUtils-PS2yJrl4.js.map
